7a98e6583e52438307ae2db3f1e39c1852cac070
Fernando Cuello Garcia <fernando.cuellogarcia@yale.edu>
2023-01-06 16:47:51 -0600

    Updated about page with a better leadership layout and inclusion of members

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -43,7 +43,7 @@
         </div>
 
         <!-- Affiliations -->
-        <div class="mx-auto px-8 max-w-7xl self-center">
+        <div class="pb-12 px-6 lg:px-8 lg:pb-18 max-w-7xl self-center">
           <p class="text-2xl text-primary text-center">Our continous support to the Yale community is in great thanks to our affiliation with
           <a class="text-primary-light font-medium underline decoration-2" href="https://ccam.yale.edu/" target="_blank" rel="noopener noreferrer">
             Yale’s Center for Collaborative Arts and Media (CCAM)
@@ -54,52 +54,54 @@
           </a>.</p>
         </div>
 
+        <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
         <!-- People  -->
         <!-- TODO: stack the rows of people -->
-        <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24 space-y-8">
-          <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Our Team</h2>
-          <div class="lg:col-span-2">
-            <ul role="list" class="flex flex-wrap justify-between space-y-12 sm:space-y-0 sm:-mt-8 lg:gap-x-8 lg:space-y-0">
-              <li v-for="person in people" :key="person.name" class="sm:py-8 flex-1">
-                <div class="space-y-4 sm:grid sm:grid-cols-8 sm:items-start sm:gap-6 sm:space-y-0 min-w-full">
-                  <div class="col-span-4 aspect-w-3 aspect-h-2 sm:aspect-w-3 sm:aspect-h-4">
-                    <img class="object-cover shadow-lg rounded-lg" :src=person.imageUrl alt="" />
-                  </div>
-                  <div class="sm:col-span-4">
-                    <div class="space-y-4">
-                      <div class="text-lg leading-6 font-medium space-y-1">
-                        <h3>{{ person.name }}</h3>
-                        <p class="text-indigo-600">{{ person.role }}</p>
-                      </div>
-                      <!-- <div class="text-lg">
-                        <p class="text-gray-500">{{ person.bio }}</p>
-                      </div> -->
-                      <ul role="list" class="flex space-x-5">
-                        <li>
-                          <a :href="person.twitterUrl" class="text-gray-400 hover:text-gray-500">
-                            <span class="sr-only">Twitter</span>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                          </a>
-                        </li>
-                        <li>
-                          <a :href="person.linkedinUrl" class="text-gray-400 hover:text-gray-500">
-                            <span class="sr-only">LinkedIn</span>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path fill-rule="evenodd" d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z" clip-rule="evenodd" />
-                            </svg>
-                          </a>
-                        </li>
-                      </ul>
+        <div class="mx-auto max-w-7xl py-12 px-6 lg:px-8 lg:py-18">
+          <div class="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
+            <div class="space-y-5 sm:space-y-4">
+              <h2 class="text-3xl font-bold tracking-tight sm:text-4xl">Meet our leadership</h2>
+              <p class="text-xl text-gray-500">The 2022-2023 board is incredibly excited to support The Cinemat through the academic year. Please reach out if you have any questions!</p>
+            </div>
+            <div class="lg:col-span-2">
+              <ul role="list" class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
+                <li v-for="person in people" :key="person.name">
+                  <div class="flex items-center space-x-4 lg:space-x-6">
+                    <img class="h-16 w-16 rounded-full lg:h-20 lg:w-20" :src="person.imageUrl" alt="" />
+                    <div class="space-y-1 text-lg font-medium leading-6">
+                      <h3>{{ person.name }}</h3>
+                      <p class="text-indigo-600">{{ person.role }}</p>
                     </div>
                   </div>
-                </div>
-              </li>
-            </ul>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
+      <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+
+    <div class="mx-auto max-w-7xl py-12 px-6 text-center lg:px-8 lg:py-18">
+      <div class="space-y-8 sm:space-y-12">
+        <div class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
+          <h2 class="text-3xl font-bold tracking-tight sm:text-4xl">Members</h2>
+          <p class="text-xl text-gray-500">The 2022-2023 memeber cohort. Take a look at this page for some information on how to become a member.</p>
+        </div>
+        <ul role="list" class="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
+          <li v-for="person in people" :key="person.name">
+            <div class="space-y-4">
+              <img class="mx-auto h-20 w-20 rounded-full lg:h-24 lg:w-24" :src="person.imageUrl" alt="" />
+              <div class="space-y-2">
+                <div class="text-xs font-medium lg:text-sm">
+                  <h3>{{ person.name }}</h3>
+                  <p class="text-indigo-600">{{ person.role }}</p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
 
 
       
