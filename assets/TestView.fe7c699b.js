4d12aed652c4290f69701ae3f6eb3313ee16788c
califernication <califernication@users.noreply.github.com>
2023-05-08 16:53:54 +0000

    deploy: a9bcf632217711fb0cc8c2efdd59f79bb61fe911

--- assets/TestView.fe7c699b.js
+++
@@ -1,1 +0,0 @@
-import{o,c as t,f as s,V as a}from"./index.1cc53d76.js";const i={__name:"TestView",setup(n){return(e,r)=>(o(),t("div",null,[s(a,{options:e.videoOptions},null,8,["options"])]))}};export{i as default};
