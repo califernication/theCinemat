6eb38e09566a995311e1319667ea6b652ee5a82d
califernication <califernication@users.noreply.github.com>
2023-08-31 16:02:13 +0000

    deploy: 4adb3efc67c61bc23637d728156e6a7ebf874440

---
+++ assets/EditOneView.1ceeca9d.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.6e35bd31.js";const r={};function t(_,c){return" hi "}var a=e(r,[["render",t]]);export{a as default};
---
+++ assets/TestView.b984e108.js
@@ -0,0 +1,1 @@
+import{o,c as t,h as s,Y as a}from"./index.6e35bd31.js";const i={__name:"TestView",setup(n){return(e,r)=>(o(),t("div",null,[s(a,{options:e.videoOptions},null,8,["options"])]))}};export{i as default};
