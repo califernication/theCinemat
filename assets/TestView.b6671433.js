6e9468203f8aeefd38361a2f5d71e1e22df08967
califernication <califernication@users.noreply.github.com>
2023-09-09 18:03:33 +0000

    deploy: 20b0afe3da30dee93b629fca4bcc8333951a6856

--- assets/TestView.b6671433.js
+++
@@ -1,1 +0,0 @@
-import{o,c as t,i as a,a1 as s}from"./index.2fc247f1.js";const c={__name:"TestView",setup(n){return(e,r)=>(o(),t("div",null,[a(s,{options:e.videoOptions},null,8,["options"])]))}};export{c as default};
